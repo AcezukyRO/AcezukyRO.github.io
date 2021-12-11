619d5457a3ed99b5c1e45eee0a8294a0096ede77
rock-on <36922874+AcezukyRockon@users.noreply.github.com>
2021-12-11 13:53:12 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+$('body').terminal({
+    // help
+    help: function () {
+        this.echo("iam \t\t iam command and pass your name as argument"
+        + "\naboutme \t About me"
+        + "\nosu \t\t View my osu stuff"
+        + "\ncontacts \t More way to contact me"
+        + "\nrock-on \t :3"
+        );
+    },
+    // iam
+    iam: function (name) {
+        this.echo('Hello, ' + name);
+    },
+    // rock-on
+    "rock-on": function () {
+        this.echo("owo? I'm here~ :3");
+    },
+    // aboutme
+    aboutme: function () {
+        this.echo('An uni student who is trying to balance between tourney staffing and study.'
+        + '\nI am also interested in games, computer stuffs and sometime in medics.'
+        );
+    },
+    // osu
+    osu: function () {
+        this.echo('An osu! player trying to get better in all gamemode (jack of all trade)'
+        + '\nosu! profile link: https://osu.ppy.sh/users/9676089'
+        );
+    },
+    // contact
+    contacts: function () {
+        this.echo('Discord: rock-on#9446'
+        + '\nTwitter: https://twitter.com/AcezukyRockon'
+        + '\nFacebook: https://www.facebook.com/AcezukyRockon/'
+        );
+    }
+}, {
+    greetings: 'Welcome to rock-on profile! Type "help" to view all commands.'
+});
